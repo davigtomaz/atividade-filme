bae93d7f8d8ed38331e709c4eaf2ae710c9afb8c
ArianeJunkes <arianejunkes312@gmail.com>
2023-03-28 16:42:47 -0300

    SOCORROO

--- App.js
+++ App.js
@@ -1,53 +1,79 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 function Card(props) {
+  const RemoveButton = ({ onPress, item, onRemove }) => {
+    return (
+      <TouchableOpacity onPress={onPress}>
+        <Text>Remover</Text>
+      </TouchableOpacity>
+    );
+  }
   return (
     <View style={styles.card}>
-      <Image style={styles.livro} source={{ uri: props.livro.capa }} />
-      <Text style={{ color: "black" }}> {props.livro.titulo} </Text>
-      <Text style={{ fontSize: 12 }}> Gênero: {props.livro.genero}</Text>
+      <Image style={styles.filme} source={{ uri: props.filme.capa }} />
+      <Text style={{ color: "black" }}> {props.filme.titulo} </Text>
+      <Text style={{ fontSize: 12 }}> Gênero: {props.filme.genero}</Text>
+      <RemoveButton  onPress={() => onRemove(Filmes.id) }/>
     </View>
   );
 }
 
 export default function App() {
-  const livros = [
+  const Filmes = [
     {
+      id: '1',
       capa: "https://img.melhoresfilmes.com.br/unsafe/320x480/https%3A%2F%2Fwww.melhoresfilmes.com.br%2Fstorage%2Fimgs%2Fposters%2F39711.jpg%3Ft%3D20230110104349",
-      titulo: 'A Baleia',
-      genero: 'Drama',
+      titulo: "A Baleia",
+      genero: "Drama",
     },
     {
+      id: '2',
       capa: "https://img.melhoresfilmes.com.br/unsafe/320x480/https%3A%2F%2Fwww.melhoresfilmes.com.br%2Fstorage%2Fimgs%2Fposters%2F37378.jpg%3Ft%3D20230304142723",
-      titulo: 'Top Gun Maverick',
-      genero: 'Ação',
+      titulo: "Top Gun Maverick",
+      genero: "Ação",
     },
     {
+      id: '3',
       capa: "https://lumiere-a.akamaihd.net/v1/images/encanto_ka_bpo_pay1_ee2c2e0c.jpeg",
-      titulo: 'Encanto',
-      genero: 'Desenho',
+      titulo: "Encanto",
+      genero: "Desenho",
     },
     {
+      id: '4',
       capa: "https://livretvnoticias.com.br/hf-conteudo/uploads/posts/2022/12/52383_ff1c4447632be7657efe4f06a8b0658c.jpg",
-      titulo: 'Terrifer',
-      genero: 'Terror',
+      titulo: "Terrifer",
+      genero: "Terror",
     },
     {
+      id: '5',
       capa: "https://bibliotecaucs.files.wordpress.com/2019/06/como-eu-era-antes-de-vocc3aa.jpg",
-      titulo: 'Como eu era antes de você',
-      genero: 'Romance',
+      titulo: "Como eu era antes de você",
+      genero: "Romance",
+    },
+    { 
+      id: '6',
+      capa: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcThhjAv6Y9BKKrJyczESuQQwW7MSBJbWuWtUiN3e5MgEv5op-ZF",
+      titulo: "A Queda",
+      genero: "Suspense",
     },
   ];
+
+  // funcao para remover da lista....
+  // find no lista pelo ID
+  // splice para remover o elemento
+
   return (
     <View>
       <View style={styles.texto}>
-      <Text >Olá Querido</Text>
+        <Text>Os Melhores Filmes da Atualidade</Text>
       </View>
       <View style={styles.conteudo}>
-        { livros.map(livro => <Card livro={livro} />)}
+        {Filmes.map((filme) => (
+          <Card filme={filme} onRemove={/*chmar a funcao que vc vai fazer passado o DI */}/>
+        ))}
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -61,28 +87,28 @@ const styles = StyleSheet.create({
   conteudo: {
     flex: 1,
     backgroundColor: "#ccc",
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    width: '100%',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "100%",
   },
   card: {
-   backgroundColor: 'gray',
-   width: '40%',
-   height: 170,
-   borderRadius: 10,
-   margin: 20,
-    },
-  livro: {
-    resizeMode: 'stretch',
-    width: '100%',
+    backgroundColor: "gray",
+    width: "40%",
+    height: 200,
+    borderRadius: 10,
+    margin: 20,
+  },
+  filme: {
+    resizeMode: "stretch",
+    width: "100%",
     height: 140,
     borderRadius: 10,
   },
   texto: {
     justifyContent: "center",
-    alignItems: 'center',
-    fontSize: 100, 
-    fontWeigth: 'bold',
-    paddingTop: "20%"
+    alignItems: "center",
+    fontSize: 100,
+    fontWeigth: "bold",
+    paddingTop: "20%",
   },
 });
